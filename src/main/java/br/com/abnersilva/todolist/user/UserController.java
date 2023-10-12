865bf8787f8a76ef3f5a83bd6ff4a42360d3e0fa
AbnerRS <abnersesisenai@gmail.com>
2023-10-12 11:21:08 -0300

    Aula 03 - Implementando segurança nos dados do usuário

--- src/main/java/br/com/abnersilva/todolist/user/UserController.java
+++ src/main/java/br/com/abnersilva/todolist/user/UserController.java
@@ -1,13 +1,15 @@
 package br.com.abnersilva.todolist.user;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -23,6 +25,11 @@ public class UserController {
        if(user != null){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário Já existe");
        }
+
+       var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+       userModel.setPassword(passwordHashred);
+       
        var userCreated = this.userRepository.save(userModel);
 
        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
